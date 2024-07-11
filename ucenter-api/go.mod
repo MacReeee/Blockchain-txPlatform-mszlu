c1d0c65a36492a7dff20083381d32b2ce00a5952
yang <2645636106@qq.com>
2024-07-11 17:31:35 +0800

    搭建用户rpc服务

---
+++ ucenter-api/go.mod
@@ -0,0 +1,3 @@
+module ucenter-api
+
+go 1.22
---
+++ ucenter/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"grpc-common/ucenter/types/register"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/core/service"
+	"github.com/zeromicro/go-zero/zrpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"ucenter/internal/config"
+	"ucenter/internal/server"
+	"ucenter/internal/svc"
+)
+
+var configFile = flag.String("f", "etc/config.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c)
+	ctx := svc.NewServiceContext(c)
+
+	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+		register.RegisterRegisterServer(grpcServer, server.NewRegisterServer(ctx))
+
+		if c.Mode == service.DevMode || c.Mode == service.TestMode {
+			reflection.Register(grpcServer)
+		}
+	})
+	defer s.Stop()
+
+	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
+	s.Start()
+}
